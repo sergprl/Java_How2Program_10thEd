9a12db9f76db544aaf653a706c40fa7672a60ff9
sergprl <speralmoreno@gmail.com>
2025-06-24 08:00:59 -0600

    improving drawing files

--- chapter_04_Exercises/src/main/java/Drawing/DrawPanel.java
+++ chapter_04_Exercises/src/main/java/Drawing/DrawPanel.java
@@ -12,10 +12,13 @@ public class DrawPanel extends JPanel {
     {
         // call paintComponent to ensure the panel displays correctly
         super.paintComponent(g);
-        drawRotations(g);
-
-
+        drawManyCurves(g, 4, 20);
     }
+
+
+    // ----------------------------------------------------------------------------------------
+    // Basic functions for testing
+    // ----------------------------------------------------------------------------------------
 
     public void drawLine(Graphics g) {
         int width = getWidth();
@@ -28,114 +31,129 @@ public class DrawPanel extends JPanel {
         g.drawLine(0, height, width, 0);
     }
 
-    public void drawManyLines(Graphics g) {
-        // there are 15 lines
-        // they end at line that goes from (0, height, width, 0)
-        int width = getWidth();
-        int height = getHeight();
 
-        int step = 15;
+    // ----------------------------------------------------------------------------------------
+    // Functions for drawing the pictures
+    // ----------------------------------------------------------------------------------------
 
-        for (int i = 0; i<=step; i++){
-            g.drawLine(0, 0, (int)((1.0/step)*i*width), (int)(height - ((1.0/step)*i*height)));
-        }
-    }
-
-    public void drawRotations(Graphics g) {
+    public void drawManyLines(Graphics g, int rotations, int steps) {
         // get variables
         int width = getWidth();
         int height = getHeight();
-        double x0, x1, y0, y1;
+
+        //                 x0t  y0t x1t  y1t
+        double[] points = { 0,   0,  0,  0};
+
         double cx = (1.0/2) * width;
         double cy = (1.0/2) * height;
-        double x0t, x1t, y0t, y1t;
 
-        int step = 20, rotation = 4;
-        for (int i = 0; i<=step; i++){
+        for (int i = 0; i<=steps; i++){
             // get points
-            x0 = 0;
-            x1 = (1.0/step)*i*width;
-            y0 = 0;
-            y1 = height - ((1.0/step)*i*height);
+            points = genSquareLines(steps, i);
 
             // draw each rotation
-            for (int j = 0; j < rotation; j++) {
-                // set up the rotation
-                double theta = Math.toRadians((360.0/rotation)*j);
-
-                // rotate (translate, then rotate, then translate back)
-                x0t = (x0 - cx) * Math.cos(theta) - (y0 - cy) * Math.sin(theta) + cx;
-                y0t = (x0 - cx) * Math.sin(theta) + (y0 - cy) * Math.cos(theta) + cy;
-
-                x1t = (x1 - cx) * Math.cos(theta) - (y1 - cy) * Math.sin(theta) + cx;
-                y1t = (x1 - cx) * Math.sin(theta) + (y1 - cy) * Math.cos(theta) + cy;
-
-                // draw line
-                g.drawLine((int)x0t, (int)y0t, (int)x1t, (int)y1t);
-
-
-            }
+            rotateLine(g, rotations, cx, cy, points[0], points[1], points[2], points[3]);
         }
 
         // for loop for each rotation
     }
 
-    public void drawCurve(Graphics g) {
+
+    // draw many curves based on rotation and number of steps
+    public void drawManyCurves(Graphics g, int rotations, int steps) {
         int width = getWidth();
         int height = getHeight();
-        double x1, y0;
 
-        double steps = 30.0;
-        double x_increments = width/steps;
-        double y_increments = height/steps;
-        int i = 0;
+        //                 x0t  y0t x1t  y1t
+        double[] points = { 0,   0,  0,  0};
 
-        while (i < steps) {
-            y0 = y_increments * i;
-            x1 = x_increments * i;
-
-            g.drawLine(0, (int)y0, (int)x1, width);
-
-            i++;
-        }
-    }
-
-    public void drawManyCurves(Graphics g) {
-        int width = getWidth();
-        int height = getHeight();
-        double x0, x1, y0, y1;
-
-        double steps = 15.0;
-        int rotations = 4;
-        double x_increments = width/steps;
-        double y_increments = height/steps;
-        double x0t, x1t, y0t, y1t;
-        int i = 0;
         double cx = (1.0/2) * width;
         double cy = (1.0/2) * height;
 
+        int i = 0;
         while (i < steps) {
-            x0 = 0;
-            y0 = y_increments * i;
-            x1 = x_increments * i;
-            y1 = width;
+            points = genCurveLines(i, steps);
 
-
-            for (int j = 0; j < rotations; j++) {
-                double theta = Math.toRadians((360.0/rotations) * j);
-
-                // perform rotation
-                x0t = (x0 - cx) * Math.cos(theta) - (y0 - cy) * Math.sin(theta) + cx;
-                y0t = (x0 - cx) * Math.sin(theta) + (y0 - cy) * Math.cos(theta) + cy;
-
-                x1t = (x1 - cx) * Math.cos(theta) - (y1 - cy) * Math.sin(theta) + cx;
-                y1t = (x1 - cx) * Math.sin(theta) + (y1 - cy) * Math.cos(theta) + cy;
-
-                g.drawLine((int)x0t, (int)y0t, (int)x1t, (int)y1t);
-            }
+            rotateLine(g, rotations, cx, cy, points[0], points[1], points[2], points[3]);
 
             i++;
         }
 
     }
+
+    // ----------------------------------------------------------------------------------------
+    // Functions for generating lines
+    // ----------------------------------------------------------------------------------------
+
+    public double[] genSquareLines(int steps, int step) {
+        // there are 15 lines
+        // they end at line that goes from (0, height, width, 0)
+        int width = getWidth();
+        int height = getHeight();
+
+        //                 x0t  y0t x1t  y1t
+        double[] points = { 0,   0,  0,  0};
+
+        points[0] = 0;
+        points[1] = 0;
+        points[2] = (1.0/steps)*step*width;
+        points[3] = height - ((1.0/steps)*step*height);
+
+        return points;
+    }
+
+    public double[] genCurveLines(int step, int steps) {
+        int width = getWidth();
+        int height = getHeight();
+
+        //                 x0t  y0t x1t  y1t
+        double[] points = { 0,   0,  0,  0};
+
+        double x_increments = width/steps;
+        double y_increments = height/steps;
+
+        points[0] = 0;
+        points[1] = y_increments * step;
+        points[2] = x_increments * step;
+        points[3] = width;
+
+        return points;
+
+    }
+
+    // ----------------------------------------------------------------------------------------
+    // Functions for rotations
+    // ----------------------------------------------------------------------------------------
+
+    // use rotate formula to draw rotated versions of the given lines
+    public void rotateLine(Graphics g, int rotations, double cx, double cy,
+                           double x0, double y0, double x1, double y1) {
+        for (int j = 0; j < rotations; j++) {
+            double theta = (360.0/rotations) * j;
+
+            double[] points = rotateFormula(theta, cx, cy, x0, x1, y0, y1);
+
+            g.drawLine((int)points[0], (int)points[1], (int)points[2], (int)points[3]);
+        }
+    }
+
+    // rotate formula
+    public double[] rotateFormula (double degrees, double cx, double cy, double x0, double x1, double y0, double y1) {
+        // set theta to rads
+        double theta = Math.toRadians(degrees);
+
+        //                 x0t  y0t x1t  y1t
+        double[] points = { 0,   0,  0,  0};
+
+        // apply formula
+        points[0] = (x0 - cx) * Math.cos(theta) - (y0 - cy) * Math.sin(theta) + cx;
+        points[1] = (x0 - cx) * Math.sin(theta) + (y0 - cy) * Math.cos(theta) + cy;
+
+        points[2] = (x1 - cx) * Math.cos(theta) - (y1 - cy) * Math.sin(theta) + cx;
+        points[3] = (x1 - cx) * Math.sin(theta) + (y1 - cy) * Math.cos(theta) + cy;
+
+        return points;
+
+    }
+
 } // end class DrawPanel
